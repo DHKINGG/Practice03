b582c765304951fbfddedc4f200ec6ff508b6179
DHKINGG <donghwi9507@gmil.com>
2022-11-12 12:00:10 +0900

    test commit2

---
+++ app/src/main/java/com/example/myapplication/Activity/ApiUrlActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.Activity
+
+import android.app.Application
+
+class ApiUrlActivity : Application() {
+
+    companion object {
+//        api URL 들 및 api 키값 여기다 써
+//        api 사용시 더미데이터들 삭제 및 API 모델로 대체
+//
+    }
+}
