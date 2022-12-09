c26ba3a3fd9ea05322c8db9630927b68bb84ad9e
DHKINGG <donghwi9507@gmil.com>
2022-12-09 23:00:55 +0900

    Map Activity 수정 및 test mark 생성 완료

--- app/src/main/java/com/example/myapplication/Activity/MapActivity.kt
+++ app/src/main/java/com/example/myapplication/Activity/MapActivity.kt
@@ -1,25 +1,21 @@
 package com.example.myapplication.Activity
 
+
+import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Looper
 import android.util.Log
 import android.view.View
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.myapplication.Model.SearchModel
-
-
 import com.example.myapplication.databinding.ActivityMapBinding
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.model.Marker
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
 
 
@@ -27,9 +23,8 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapBinding
     private lateinit var mapView: MapView
     private lateinit var naverMap: NaverMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationSource: FusedLocationSource
-
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,8 +34,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(view)
 
 
-
-
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
 
         mapView = binding.mapNaver
@@ -51,9 +45,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
         binding.tvBookHospitalName.text = mapBottomInfo.hospitalName
         binding.tvSearchResultHospitalAddress.text = mapBottomInfo.hospitalAddress
-        binding.ivSearchLeftArrow.setOnClickListener {finish()}
-
-
+        binding.ivSearchLeftArrow.setOnClickListener { finish() }
 
 
     }
@@ -66,39 +58,92 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
 
-
     override fun onMapReady(map: com.naver.maps.map.NaverMap) {
         val naverMap = map
         val uiSettings = naverMap.uiSettings
 
-        naverMap.maxZoom = 18.0
-        naverMap.minZoom = 5.0
+        naverMap.minZoom = 10.0
+
+
 
 
         naverMap.setOnMapClickListener { point, coord ->
-            Log.d("QQQQ", "실행완료 ")
             binding.clBottomInfo.visibility = View.GONE
         }
-
-
-        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.474113,127.110258))
-        naverMap.moveCamera(cameraUpdate)
 
         uiSettings.isLocationButtonEnabled = false
         uiSettings.isZoomControlEnabled = false
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
-
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
         naverMap.locationSource = locationSource
-        val locationOverlay = naverMap.locationOverlay
-        locationOverlay.isVisible = true
-        locationOverlay.position =LatLng(37.474113,127.110258)
 
-        binding.searchResultFabMap.map = naverMap
 
+        var currentLocation: Location?
+
+
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            return
+        }
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            currentLocation = location
+
+            naverMap.locationOverlay.run {
+                isVisible = true
+                position = LatLng(currentLocation!!.latitude, currentLocation!!.longitude)
+            }
+
+            val cameraUpdate = CameraUpdate.scrollTo(
+                LatLng(
+                    currentLocation!!.latitude,
+                    currentLocation!!.longitude
+                )
+            )
+            naverMap.moveCamera(cameraUpdate)
+
+            val cameraUpdateToSeoul =
+                CameraUpdate.scrollTo(LatLng(37.47549389999994, 127.10558120000012))
+
+            binding.searchResultFabMap.setOnClickListener {
+                naverMap.moveCamera(cameraUpdateToSeoul)
+            }
+
+            val marker = Marker()
+            marker.position = LatLng(
+                naverMap.cameraPosition.target.latitude,
+                naverMap.cameraPosition.target.longitude
+            )
+            marker.map = naverMap
+            val marker2 = Marker()
+            val marker3 = Marker()
+            val marker4 = Marker()
+            val marker5 = Marker()
+
+            marker2.position = LatLng(37.47545880000004, 127.10067899999981)
+            marker3.position = LatLng(37.473568699999575, 127.1036674999995)
+            marker4.position = LatLng(37.47323779999989, 127.10212769999973)
+            marker5.position = LatLng(37.47415849999969, 127.11026200000008)
+
+            marker2.map = naverMap
+            marker3.map = naverMap
+            marker4.map = naverMap
+            marker5.map = naverMap
+
+
+        }
 
 
     }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -109,17 +154,13 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         if (requestCode != LOCATION_PERMISSION_REQUEST_CODE)
             return
 
-        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)){
-            if (!locationSource.isActivated){ // 권한 거부시 위치추적 x
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부시 위치추적 x
                 naverMap.locationTrackingMode = LocationTrackingMode.None
             }
             return
         }
     }
-
-
-
-
 
 
     override fun onStart() {
@@ -159,11 +200,9 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
 
-
-    companion object{
+    companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
-
 
 
 }
